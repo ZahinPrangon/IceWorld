d346a9efd0de421c2e6487b2e123cb960317cdf1
ZahinPrangon <zhprangon@mun.ca>
2023-10-10 05:41:08 +0600

    adding custom tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,8 @@
 
 // Import the functions you need from the SDKs you need
 
-import Script from "next/script";
-
 import FacebookPixel from "@/components/FacebookPixel/FacebookPixel";
+import GoogleTagManager from "@/components/GoogleTagManager/GoogleTagManager";
 
 import { AuthProvider } from "./(root)/Providers";
 
@@ -41,15 +40,6 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-        `}
-      </Script>
       <noscript>
         <img
           alt="facebook-pixel"
@@ -62,15 +52,15 @@ export default function RootLayout({
           src="https://www.facebook.com/tr?id=875994250211128&ev=PageView&noscript=1"
         />
       </noscript>
-
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+        }}
+      />
       <body>
         <AuthProvider>{children}</AuthProvider>
         <FacebookPixel />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
-          }}
-        />
+        <GoogleTagManager />
       </body>
     </html>
     // </ClerkProvider>
